7f4c7f90a36a79fdd141baa5f71e7a18d7dcd237
Aayushi Modi <avmodi@cs.washington.edu>
2022-01-26 15:56:16 -0800

    rename Main.java

--- src/CalculateStartingWord.java
+++ src/CalculateStartingWord.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class Main {
+public class CalculateStartingWord {
 
     public static void main(String[] args) throws FileNotFoundException {
 
